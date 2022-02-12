e1233fba37131faf23b59cd5ee3609792e4c8397
Buronov <boronovjonibek@gmail.com>
2022-02-12 23:02:26 +0900

    feat: Sign Up Page was Createdgit add .!

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
-
+const bodyParser = require("body-parser")
+const request = require("request")
 const app = express();
 
 app.use(express.static("public"))
 
+app.use(bodyParser.urlencoded({extended: true}))
+
+
 app.get("/", (req, res) => {
-    res.send("You are here!!!")
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", (req, res) => {
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    console.log(firstName, lastName, email, password)
 })
 
 app.listen(3003, () => {
