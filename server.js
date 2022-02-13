a5524921166d48e600f59c5ed5713181ff82657c
Buronov <boronovjonibek@gmail.com>
2022-02-13 11:27:28 +0900

    feat: Create mailchimp's DB, Write a date to DB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const request = require("request")
+const https = require("https")
+
 const app = express();
 
 app.use(express.static("public"))
@@ -13,14 +15,59 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-    var firstName = req.body.fname;
-    var lastName = req.body.lname;
-    var email = req.body.email;
-    var password = req.body.password;
+    const fullName = req.body.fname;
+    const email = req.body.email;
+    const password = req.body.password;
+    const phoneNumber = req.body.number;
 
-    console.log(firstName, lastName, email, password)
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fullName,
+                    PPWP_PWD: password,
+                    PHONE: phoneNumber
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://*.api.mailchimp.com/3.0/lists/*"
+
+    const options = {
+        method: "POST",
+        auth: "jonik:*"
+    }
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
 })
 
 app.listen(3003, () => {
     console.log("Your Server Is Running On 3003 Port!!!")
 })
+
+// API KEY
+// dd11e463d63a643d6fcbca38857d9bdc-us14
+
+// LIST ID
+// e9556c4180
